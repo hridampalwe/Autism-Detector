7bd594b5de1ca4e7a52daf2bf7b5088bb17b6c98
Hridam Palwe <palwehridam@gmail.com>
2023-10-28 11:11:25 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/views/login_view.dart';
+import 'package:flutter_application_1/views/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                print(FirebaseAuth.instance.currentUser);
+                return const Text("Done");
+              default:
+                return const Text("Loading..");
+            }
+          },
+        ));
+  }
+}
